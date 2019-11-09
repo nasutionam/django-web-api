40228b1d17811809551212c584b88a3e8ec8e3b6
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-09 17:12:38 +0700

    GCBV for List and Detail API Article

--- newsapi/news/api/urls.py
+++ newsapi/news/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import article_list_create_api_view, article_detail_create_api_view
+#from .views import article_list_create_api_view, article_detail_create_api_view
+from .views import ArticleDetailAPIView, ArticleListCreateAPIView
+
 
 urlpatterns = [
-    path("articles/", article_list_create_api_view, name="article-list"),
-    path("articles/<int:pk>/", article_detail_create_api_view, name="article-detail")
+    # path("articles/", article_list_create_api_view, name="article-list"),
+    # path("articles/<int:pk>/", article_detail_create_api_view, name="article-detail")
+
+    path("articles/", ArticleListCreateAPIView.as_view(), name="article-list"),
+    path("articles/<int:pk>/", ArticleDetailAPIView.as_view(), name="article-detail")
 ]
