a0a2d87af5617399dd7110d8e08ce0a6885e0e35
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-09 10:16:43 +0700

    database setting & installed app

--- newsapi/newsapi/settings.py
+++ newsapi/newsapi/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'news'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'newsapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newsapi',
+        'USER': 'nam',
+        'PASSWORD': 'Testing100x',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
