e7006839fbabeade567757e71c21fd32473baa93
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-12 20:03:24 +0700

    quotes app initiation

--- quotesapi/quotesapi/settings.py
+++ quotesapi/quotesapi/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'quotesapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quotesapi',
+        'USER': 'nam',
+        'PASSWORD': 'Testing100x',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
