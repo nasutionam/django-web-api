ac353a253411d7c2c60d078ebd8c3dd992019dd6
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-12 07:51:39 +0700

    added permission role for user

--- ebooksapi/ebooks/api/views.py
+++ ebooksapi/ebooks/api/views.py
@@ -2,10 +2,11 @@ from rest_framework import generics
 from rest_framework import mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework import permissions
+from rest_framework.exceptions import ValidationError
 
 from ..models import Ebook, Review
 from .serializers import EbookSerializer, ReviewSerializer
-from .permissions import IsAdminUserOrReadOnly
+from .permissions import IsAdminUserOrReadOnly, IsReviewAuthorOrReadOnly
 
 # class EbookListCreateAPIView(mixins.ListModelMixin,
 #                             mixins.CreateModelMixin,
@@ -34,13 +35,20 @@ class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 class ReviewCreateAPIView(generics.CreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAdminUserOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         ebook_pk = self.kwargs.get('ebook_pk')
         ebook = get_object_or_404(Ebook, pk=ebook_pk)
-        serializer.save(ebook=ebook)
+        review_author = self.request.user
+
+        review_queryset = Review.objects.filter(ebook=ebook, review_author=review_author)
+
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this ebook")
+        serializer.save(ebook=ebook, review_author=review_author)
 
 class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsReviewAuthorOrReadOnly]
--- ebooksapi/ebooks/models.py
+++ ebooksapi/ebooks/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 
+from django.contrib.auth.models import User
+
 class Ebook(models.Model):
     title = models.CharField(max_length=120)
     author = models.CharField(max_length=50)
@@ -13,7 +15,7 @@ class Ebook(models.Model):
 class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    review_author = models.CharField(max_length=8, blank=True, null=True)
+    review_author = models.ForeignKey(User, on_delete=models.CASCADE)
     review = models.TextField(blank=True, null=True)
     rating = models.PositiveIntegerField()
     ebook = models.ForeignKey(Ebook, on_delete=models.CASCADE, related_name='reviews')
