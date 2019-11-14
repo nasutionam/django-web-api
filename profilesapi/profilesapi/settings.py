8aeb1bf1a75981c351cb87b87fff60b1fbe96570
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-14 20:11:30 +0700

    profiles app initiation

--- profilesapi/profilesapi/settings.py
+++ profilesapi/profilesapi/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'profiles'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'profilesapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'profiles',
+        'USER': 'nam',
+        'PASSWORD': 'Testing100x',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
