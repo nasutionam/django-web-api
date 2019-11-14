58823e76b9d74eecff3e87d8439edfe4b7a11863
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-14 21:43:36 +0700

    added auth, sessionauth and tokenauth

--- profilesapi/profilesapi/settings.py
+++ profilesapi/profilesapi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'profiles'
 ]
 
@@ -127,3 +128,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'uploads'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
