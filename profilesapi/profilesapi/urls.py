05b6491033ed9f0cdead29b59062f0eec461c72d
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-15 07:18:32 +0700

    added django allauth

--- profilesapi/profilesapi/urls.py
+++ profilesapi/profilesapi/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('profiles.api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/rest-auth/', include('rest_auth.urls'))
+    path('api/rest-auth/', include('rest_auth.urls')),
+    path('api/rest-auth/registration/', include('rest_auth.registration.urls'))
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
