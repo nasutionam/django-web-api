8a9b4d6207a122892846aee0048acccc28ca4975
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-16 13:38:49 +0700

    added test case for registration method

--- profilesapi/profiles/tests.py
+++ profilesapi/profiles/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+
+from .api.serializers import ProfileSerializer
+from .models import Profile
+
+class RegistrationTestCase(APITestCase):
+    
+    def test_registration(self):
+        data = { 
+            "username": "testcase",
+            "email" : "testcase@localhost.net",
+            "password1": "Testing100x",
+            "password2": "Testing100x"
+        }
+
+        response = self.client.post("/api/rest-auth/registration/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.json())
+
